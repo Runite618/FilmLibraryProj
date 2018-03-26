74d6dee0a9a0ed9883372ab315b41ceb364288b5
Matthew Mo <matthew_mo@hotmail.co.uk>
2018-03-26 15:35:34 +0100

    Stores data on a new movie

--- FilmLibraryProj/Movie.cs
+++ FilmLibraryProj/Movie.cs
@@ -8,14 +8,44 @@ namespace FilmLibraryProj
 {
     public class Movie
     {
-        private String name { get; set; }
-        private String genre { get; set; }
-        private int rating { get; set; }
+        private String name;
+        private String genre;
+        private int rating;
 
         public Movie(String name, String genre, int rating)
         {
             this.name = name;
             this.genre = genre;
+            this.rating = rating;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+
+        public void setName(String name)
+        {
+            this.name = name;
+        }
+
+        public String getGenre()
+        {
+            return genre;
+        }
+
+        public void setGenre(String genre)
+        {
+            this.genre = genre;
+        }
+
+        public int getRating()
+        {
+            return rating;
+        }
+
+        public void setRating(int rating)
+        {
             this.rating = rating;
         }
     }
--- FilmLibraryProj/Program.cs
+++ FilmLibraryProj/Program.cs
@@ -10,7 +10,40 @@ namespace FilmLibraryProj
     {
         static void Main(string[] args)
         {
+            String movieName, movieGenre, exitFlag;
+            int movieRating = 0;
+            List<Movie> movies = new List<Movie>();
 
+            while (true)
+            {
+                Console.WriteLine("Please enter a movie name, genre and rating");
+                Console.WriteLine("Movie name:");
+                movieName = Console.ReadLine();
+                Console.WriteLine("Movie genre:");
+                movieGenre = Console.ReadLine();
+                Console.WriteLine("Movie rating: ");
+                Int32.TryParse(Console.ReadLine(), out movieRating);
+                Movie movie = new Movie(movieName, movieGenre, movieRating);
+                movies.Add(movie);
+                Console.WriteLine("Do you wish to exit program and display list of movies? [y/n]");
+                exitFlag = Console.ReadLine();
+                if(exitFlag == "y")
+                {
+                    break;
+                }
+                else
+                {
+                    continue;
+                }
+            }
+
+            Console.WriteLine("List of movies:");
+
+            foreach (Movie movie in movies) {
+                Console.WriteLine(movie.getName() + ", " + movie.getGenre() + ", " + movie.getRating());
+            }
+
+            Console.ReadLine();
         }
     }
 }
