c51688ba2f38e4c9825f04cbaffb6a986a425db6
Matthew Mo <matthew_mo@hotmail.co.uk>
2018-04-10 18:54:46 +0100

    Changed around structure of file & added in connection string

--- FilmLibraryProj/Program.cs
+++ FilmLibraryProj/Program.cs
@@ -1,33 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FilmLibraryProj
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        private String movieName, movieGenre, exitFlag;
+        private String connString = "server = localhost; user id = root; persistsecurityinfo = True; database = film";
+        private SqlConnection connection;
+        private int movieRating = 0;
+        private List<Movie> movies = new List<Movie>();
+
+        public static void Main(string[] args)
         {
-            String movieName, movieGenre, exitFlag;
-            int movieRating = 0;
-            List<Movie> movies = new List<Movie>();
+            Program program = new Program();
 
             while (true)
             {
                 Console.WriteLine("Please enter a movie name, genre and rating");
                 Console.WriteLine("Movie name:");
-                movieName = Console.ReadLine();
+                program.movieName = Console.ReadLine();
                 Console.WriteLine("Movie genre:");
-                movieGenre = Console.ReadLine();
+                program.movieGenre = Console.ReadLine();
                 Console.WriteLine("Movie rating: ");
-                Int32.TryParse(Console.ReadLine(), out movieRating);
-                Movie movie = new Movie(movieName, movieGenre, movieRating);
-                movies.Add(movie);
+                Int32.TryParse(Console.ReadLine(), out program.movieRating);
+                Movie movie = new Movie(program.movieName, program.movieGenre, program.movieRating);
+                program.movies.Add(movie);
                 Console.WriteLine("Do you wish to exit program and display list of movies? [y/n]");
-                exitFlag = Console.ReadLine();
-                if(exitFlag == "y")
+                program.exitFlag = Console.ReadLine();
+                if(program.exitFlag == "y")
                 {
                     break;
                 }
@@ -39,8 +44,9 @@ namespace FilmLibraryProj
 
             Console.WriteLine("List of movies:");
 
-            foreach (Movie movie in movies) {
+            foreach (Movie movie in program.movies) {
                 Console.WriteLine(movie.getName() + ", " + movie.getGenre() + ", " + movie.getRating());
+
             }
 
             Console.ReadLine();
